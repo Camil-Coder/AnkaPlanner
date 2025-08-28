5f47af62df771f2201dfb3f10b923e33433be7e6
unknown <camiloAnka@hotmail.com}>
2025-08-27 22:16:27 -0500

    Configuracion para desplegar

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     host: true,
     allowedHosts: ['anka']
-  }
+  },
+  base: '/ankaplanner/',   // 👈 añadimos esta línea
 })
